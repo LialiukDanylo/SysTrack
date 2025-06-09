551e791cfad063a38c797ab4c9b4b57748d1ade9
Lialiuk Danylo <lialiukdanylo@gmail.com>
2025-06-09 15:39:39 +0200

    feat(client): add connection to SignalR hub

--- SysTrack.Client/App.xaml.cs
+++ SysTrack.Client/App.xaml.cs
@@ -1,14 +1,28 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace SysTrack.Client
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private readonly string _groupId = "TestGroup";
+        private readonly bool isClient = true;
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5265/hub")
+                .WithAutomaticReconnect()
+                .Build();
+
+            await connection.StartAsync();
+            Console.WriteLine("Client Connected.");
+            await connection.InvokeAsync("JoinRoom", _groupId, isClient);
+
+            base.OnStartup(e);
+        }
     }
 
 }
