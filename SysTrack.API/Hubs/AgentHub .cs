810a7df93806c69a029551c1e56ef404ddd16c39
Lialiuk Danylo <lialiukdanylo@gmail.com>
2025-06-10 12:46:57 +0200

    feat(api): use connection manager to start/stop metrics

--- SysTrack.API/Hubs/AgentHub .cs
+++ SysTrack.API/Hubs/AgentHub .cs
@@ -1,13 +1,46 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using System.Collections.Concurrent;
+using SysTrack.API.Services;
+using SysTrack.API.Services.Interfaces;
 
 namespace SysTrack.API.Hubs
 {
     public class AgentHub : Hub
     {
+        private readonly IConnectionManager _connectionManager;
+        public AgentHub(IConnectionManager connectionManager) 
+        { 
+            _connectionManager = connectionManager;
+        }
+        
         public async Task JoinGroup(string groupId, bool isClient)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, groupId);
+
+            if (isClient)
+            {
+                Console.WriteLine($"[{Context.ConnectionId}] client joined {groupId}");
+                _connectionManager.AddClient(groupId, Context.ConnectionId);
+                await StartMetrics(groupId);
+            }
+            else
+            {
+                Console.WriteLine($"[{Context.ConnectionId}] agent joined {groupId}");
+            }
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            if (_connectionManager.IsClient(Context.ConnectionId))
+            {
+                var groupId = _connectionManager.GetGroupId(Context.ConnectionId);
+                _connectionManager.RemoveClient(Context.ConnectionId);
+                if (_connectionManager.GetClientsCount(groupId) == 0)
+                {
+                    _ = StopMetrics(groupId);
+                }
+            }
+
+            await base.OnDisconnectedAsync(exception);
         }
 
         public async Task StartMetrics(string groupId)
