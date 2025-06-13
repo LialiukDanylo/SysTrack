1c069ac42b78d2eb92a6993ecf22949209c38e6f
Lialiuk Danylo <lialiukdanylo@gmail.com>
2025-06-13 21:30:08 +0200

    feat(client): add UserData model

---
+++ SysTrack.Client/Models/UserData.cs
@@ -0,0 +1,13 @@
+﻿namespace SysTrack.Client.Models
+{
+    public record UserData
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public UserData(string Name = "", string Password = "")
+        { 
+            this.Name = Name;
+            this.Password = Password;
+        }
+    };
+}
