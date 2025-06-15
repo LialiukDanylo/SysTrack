48a675275bc9f841e1e8f405d1117eaa0a55c283
Lialiuk Danylo <lialiukdanylo@gmail.com>
2025-06-15 19:38:39 +0200

    feat(client): add basic login page and ViewModel

---
+++ SysTrack.Client/ViewModels/SignInPageVM.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using SysTrack.Client.Models;
+using SysTrack.Client.MVVM;
+using SysTrack.Client.Services;
+using SysTrack.Shared.Models;
+
+namespace SysTrack.Client.ViewModels
+{
+    internal class SignInPageVM : ViewModelBase
+    {
+        private UserData _data;
+        public UserData Data
+        {
+            get { return _data; }
+            set
+            {
+                _data = value;
+                OnPropetryChanged();
+            }
+        }
+        public RelayCommand SignInCommand => new RelayCommand(execute => SignIn()); 
+
+        public SignInPageVM()
+        {
+            _data = new UserData();
+        }
+
+        private async void SignIn()
+        {
+            var loginData = new LoginRequest
+            {
+                Name = _data.Name,
+                Password = _data.Password
+            };
+            var auth = new AuthService(new System.Net.Http.HttpClient());
+            var response = await auth.SignIn(loginData);
+
+            Debug.WriteLine("Response:");
+            Debug.WriteLine(response.Success);
+            Debug.WriteLine(response.Token + "\n");
+        }
+    }
+}
---
+++ SysTrack.Client/Views/SignInPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using SysTrack.Client.ViewModels;
+
+namespace SysTrack.Client.Views
+{
+    public partial class SignInPage : Page
+    {
+        public SignInPage()
+        {
+            
+            SignInPageVM vm = new SignInPageVM();
+            DataContext = vm;
+            InitializeComponent();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is SignInPageVM vm)
+            {
+                vm.Data.Password = PasswordBox.Password;
+            }
+        }
+    }
+}
