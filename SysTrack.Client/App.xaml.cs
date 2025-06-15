43f75f636c87f5757f4a3c7133debcb25263b863
Lialiuk Danylo <lialiukdanylo@gmail.com>
2025-06-15 12:16:57 +0200

    feat(client): manage MainWindow startup in App.xaml.cs

--- SysTrack.Client/App.xaml.cs
+++ SysTrack.Client/App.xaml.cs
@@ -1,54 +1,15 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Diagnostics;
-using System.Windows;
-using Microsoft.AspNetCore.SignalR.Client;
-using SysTrack.Shared.Models;
+﻿using System.Windows;
 
 namespace SysTrack.Client
 {
     public partial class App : Application
     {
-        private readonly string _groupId = "TestGroup";
-        private readonly bool isClient = true;
-
         protected override async void OnStartup(StartupEventArgs e)
         {
-            var connection = new HubConnectionBuilder()
-                .WithUrl("http://localhost:5265/hub")
-                .WithAutomaticReconnect()
-                .Build();
-
-            connection.Reconnected += async (connectionId) =>
-            {
-                await connection.InvokeAsync("JoinGroup", _groupId, isClient);
-            };
-
-            connection.On<MetricsData>("ReceiveMetrics", (metrics) =>
-            {
-                Debug.WriteLine($"{metrics.Name}\nCPU: {metrics.CpuUsage}%\nGPU: {metrics.GpuUsage}%");
-            });
-
-            _ = Task.Run(async () =>
-            {
-                while (true)
-                {
-                    try
-                    {
-                        await connection.StartAsync();
-                        await connection.InvokeAsync("JoinGroup", _groupId, isClient);
-                        break;
-                    }
-                    catch
-                    {
-                        await Task.Delay(5000);
-                    }
-                }
-            });
-
             base.OnStartup(e);
-            
+
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
         }
     }
-
 }
