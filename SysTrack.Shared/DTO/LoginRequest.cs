8e0c8790f199b43e2c5eeb1903e3776a7b81fabd
Lialiuk Danylo <lialiukdanylo@gmail.com>
2025-06-16 08:30:05 +0200

    feat(shared): rename Models to DTO and add new types

---
+++ SysTrack.Shared/DTO/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace SysTrack.Shared.Models
+{
+    public record LoginRequest
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ SysTrack.Shared/DTO/LoginResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace SysTrack.Shared.Models
+{
+    public class LoginResponse
+    {
+        public bool Success { get; set; }
+        public string Token { get; set; }
+    }
+}
