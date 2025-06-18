60414322667b32eaf5985a99f5beb0495b001d23
Lialiuk Danylo <lialiukdanylo@gmail.com>
2025-06-18 12:39:34 +0200

    feat(api): add authentication controller

---
+++ SysTrack.API/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SysTrack.Shared.Models;
+
+namespace SysTrack.API.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthController : Controller
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            Console.WriteLine("Login:");
+            Console.WriteLine(request.Name);
+            Console.WriteLine(request.Password);
+
+            var responce = new LoginResponse
+            {
+                Success = true,
+                Token = "MegaTokennasjckdancjkandacjklncjkasldacnasdnaca"
+            };
+            return StatusCode(200, responce);
+        }
+    }
+}
